eaf505cf83dc644efa7d269016caefb6604007bb
Ubuntu <ubuntu@ip-172-31-3-122.ca-central-1.compute.internal>
2023-03-30 12:11:35 +0000

    minor code updates

--- frappe_m365/controllers/file_controller.py
+++ frappe_m365/controllers/file_controller.py
@@ -7,76 +7,58 @@ GROUPS = "M365 Groups"
 
 
 def file_upload(doc, method):
-    try:
-        doctype = doc.attached_to_doctype
-        docname = doc.attached_to_name
-        group_doc, group_id, filepath = None, None, None
+    doctype = doc.attached_to_doctype
+    docname = doc.attached_to_name
+    group_doc, group_id, filepath = None, None, None
 
-        if (doctype and docname and method == "after_insert" and frappe.db.exists(M365)):
-            group_settings = frappe.get_single(M365)
-            if group_settings.enable_file_sync:
-                group_doc, group_id = get_group_doc(doctype, group_settings)
-                filepath = get_file_path(doc)
+    if (doctype and docname and method == "after_insert" and frappe.db.exists(M365)):
+        group_settings = frappe.get_single(M365)
+        if group_settings.enable_file_sync:
+            group_doc, group_id = get_group_doc(doctype, group_settings)
+            filepath = get_file_path(doc)
 
-        if (group_doc and group_id and filepath):
-            gp_doc = frappe.get_doc(GROUPS, group_doc)
-            frappe.enqueue(
-                "frappe_m365.utils.sharepoint.trigger_sharepoint", queue="long",
-                request="UPLOAD", group=gp_doc, doctype=doctype, docname=docname,
-                filepath=filepath, filedoc=doc.name, timeout=-1
-            )
-    except Exception as e:
-        frappe.log_error("event call", e)
-        frappe.throw(frappe.get_traceback())
+    if (group_doc and group_id and filepath):
+        gp_doc = frappe.get_doc(GROUPS, group_doc)
+        frappe.enqueue(
+            "frappe_m365.utils.sharepoint.trigger_sharepoint", queue="long",
+            request="UPLOAD", group=gp_doc, doctype=doctype, docname=docname,
+            filepath=filepath, filedoc=doc.name, timeout=-1
+        )
 
 
 # preparing complete file path
 def get_file_path(doc):
-    try:
-        folder_path = "/private/files/" if doc.is_private else "/public/files/"
-        filepath = os.path.abspath(os.curdir) + "/" + \
-            frappe.utils.cstr(frappe.local.site) + \
-            folder_path + doc.file_name
-        return filepath
-    except Exception as e:
-        frappe.log_error("file path error", e)
-        return None
+    path = "private/files" if doc.is_private else "public/files"
+    filepath = os.path.abspath(os.curdir) + "/" + frappe.get_site_path(path, doc.file_name)
+    return filepath
 
 
 # return M365 group doc based on settings in M365 Groups Settings doctype
 def get_group_doc(doctype, group_settings):
-    try:
-        role, group, group_id = None, None, None
-        user = frappe.session.user
-        user_roles = frappe.get_roles(user)
-        module = frappe.db.get_value("DocType", doctype, "module")
+    role, group, group_id = None, None, None
+    user = frappe.session.user
+    user_roles = frappe.get_roles(user)
+    module = frappe.db.get_value("DocType", doctype, "module")
 
-        for entry in group_settings.module_settings:
-            if (module == entry.module and user in get_group_members(entry.default_group)):
-                role = entry.role
-                group = entry.default_group
-                group_id = frappe.db.get_value(GROUPS, entry.default_group, "m365_group_id")
+    for entry in group_settings.module_settings:
+        if (module == entry.module and user in get_group_members(entry.default_group)):
+            role = entry.role
+            group = entry.default_group
+            group_id = frappe.db.get_value(GROUPS, entry.default_group, "m365_group_id")
 
-        if ((role and group and role in user_roles) or (not role and group)):
-            return group, group_id
-        elif (user in get_group_members(group_settings.default_m365_group)):
-            group_id = frappe.db.get_value(GROUPS, group_settings.default_m365_group, "m365_group_id")
-            return group_settings.default_m365_group, group_id
-        else:
-            return group, group_id
-    except Exception as e:
-        frappe.log_error("group doc error", e)
-        return None, None
+    if ((role and group and role in user_roles) or (not role and group)):
+        return group, group_id
+    elif (user in get_group_members(group_settings.default_m365_group)):
+        group_id = frappe.db.get_value(GROUPS, group_settings.default_m365_group, "m365_group_id")
+        return group_settings.default_m365_group, group_id
+    else:
+        return group, group_id
 
 
 def get_group_members(group):
-    try:
-        users = frappe.db.get_list(
-            "M365 Groups Member",
-            {"parent": group, "parenttype": 'M365 Groups'},
-            "user", ignore_permissions=True
-        )
-        return [user.user for user in users]
-    except Exception as e:
-        frappe.log_error("group members error", e)
-        return []
+    users = frappe.db.get_list(
+        "M365 Groups Member",
+        {"parent": group, "parenttype": 'M365 Groups'},
+        "user", ignore_permissions=True
+    )
+    return [user.user for user in users]
